43aa09f16593392204f38d63e7cd223771518466
Vinicius Vinhas <vinhas@cdts.fiocruz.br>
2017-05-12 16:22:37 -0300

    Update constants file

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,15 @@
 /**
  * Constants used by Redux Bolt
- * 
- * @type object
  */
-module.exports = {
-  actions: {
-    connect: 'Bolt/Connect',
-    wide: 'Bolt/Wide_Action',
-    channel: 'Bolt/Channel_Action',
-    joinChannel: 'Bolt/Join_Channel',
-    leaveChannel: 'Bolt/Leave_Channel'
-  },
-  events: {
-    send: 'Bolt/Send',
-    receive: 'Bolt/Receive'
-  }
+export const actions = {
+  connect: 'Bolt/Connect',
+  wide: 'Bolt/Wide_Action',
+  channel: 'Bolt/Channel_Action',
+  joinChannel: 'Bolt/Join_Channel',
+  leaveChannel: 'Bolt/Leave_Channel'
+}
+
+export const events = {
+  send: 'Bolt/Send',
+  receive: 'Bolt/Receive'
 }
